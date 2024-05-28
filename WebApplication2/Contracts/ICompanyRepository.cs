983860efe0bdea3375c065d61b254a3229070aad
Pratibha Pandey <ppratibhapandey53@gmail.com>
2024-05-28 15:01:22 +0530

    Add project files.

---
+++ WebApplication2/Contracts/ICompanyRepository.cs
@@ -0,0 +1,14 @@
+﻿using WebApplication2.Dto;
+using WebApplication2.Entities;
+
+namespace WebApplication2.Contracts
+{
+    public interface ICompanyRepository
+    {
+        public Task<IEnumerable<Company>> GetCompanies();
+        public Task<Company> GetCompany(int id);
+        public Task<Company> CreateCompany(CompanyForCreationDto company);
+        public Task UpdateCompany(int id, CompanyForUpdateDto company);
+        public Task DeleteCompany(int id);
+    }
+}
---
+++ WebApplication2/Entities/Employee.cs
@@ -0,0 +1,11 @@
+﻿namespace WebApplication2.Entities
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Position { get; set; }
+        public int CompanyId { get; set; }
+    }
+}
